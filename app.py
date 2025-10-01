a4f176f42faa118be09b1e569a7ac909de6c58e1
Juan Manuel Velásquez Terreros <135066758+JuanMa0912@users.noreply.github.com>
2025-10-01 11:12:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ c1, c2, c3 = st.columns([2,1,1])
 with c1:
     date_range = st.date_input("Rango de fechas (YYYY-MM-DD)", value=(min_d, max_d), format="YYYY-MM-DD")
 with c2:
-    limit = st.number_input("Límite de ítems", min_value=1, max_value=50, value=10, step=1)
+    limit = st.number_input("Límite de ítems", min_value=1, max_value=10, value=10, step=1)
 with c3:
     footer_label = st.text_input("Etiqueta de total", value="Acum. Rango:")
 
