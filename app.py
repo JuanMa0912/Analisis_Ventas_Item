699ff88669a314b92fcdb354250cb9656560ecf9
Juan Manuel Velásquez Terreros <135066758+JuanMa0912@users.noreply.github.com>
2025-10-07 12:42:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ with pd.ExcelWriter(output_excel, engine="xlsxwriter") as writer:
     worksheet.conditional_format(0, last_col, last_row, last_col, {"type": "no_errors", "format": fmt_border_ext})
 
 # Botones lado a lado
-b1, b2, _ = st.columns([1, 1, 6])
+b1, b2 = st.columns([1, 1])
 with b1:
     st.download_button(
         "💾 Descargar Excel",
